ab994b89c168cb19f7d30efbb1de0aa581e87cbf
Dzmitry Fedarovich <dmitryf8@gmail.com>
2019-06-25 20:22:01 +0300

    Добавлено активити DZ4

---
+++ app/src/main/java/by/itacademy/pvt/homework/DZ4Activity.kt
@@ -0,0 +1,12 @@
+package by.itacademy.pvt.homework
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DZ4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz4)
+    }
+}
