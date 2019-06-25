45571b6e84f77fddb6bec5bd81afa5a2383abebb
Dzmitry Fedarovich <dmitryf8@gmail.com>
2019-06-25 22:02:45 +0300

    Домашнее задание 4 выполнено

--- app/src/main/java/by/itacademy/pvt/homework/MainActivity.kt
+++ app/src/main/java/by/itacademy/pvt/homework/MainActivity.kt
@@ -9,44 +9,63 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         findViewById<View>(R.id.dz0Button)
             .setOnClickListener {
                 startDZ0()
             }
+
         findViewById<View>(R.id.dz1Button)
             .setOnClickListener {
                 startDZ1()
             }
+
         findViewById<View>(R.id.dz2Button)
             .setOnClickListener {
                 startDZ2()
             }
+
         findViewById<View>(R.id.dz2loginButton)
             .setOnClickListener {
                 startDZ2Login()
             }
+
         dz3Button.setOnClickListener {
             startDZ3()
         }
+
+        dz4Button.setOnClickListener {
+            startDZ4()
+        }
     }
+
     private fun startDZ0() {
         val intent = Intent(this, DZ0Activity::class.java)
         startActivity(intent)
     }
+
     private fun startDZ1() {
         val intent = Intent(this, DZ1Activity::class.java)
         startActivity(intent)
     }
+
     private fun startDZ2() {
         val intent = Intent(this, DZ2Activity::class.java)
         startActivity(intent)
     }
+
     private fun startDZ2Login() {
         val intent = Intent(this, DZ2LoginActivity::class.java)
         startActivity(intent)
     }
+
     private fun startDZ3() {
         val intent = Intent(this, DZ3Activity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startDZ4() {
+        val intent = Intent(this, DZ4Activity::class.java)
         startActivity(intent)
     }
 }
