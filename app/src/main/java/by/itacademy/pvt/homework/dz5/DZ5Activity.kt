b870af9dae0faece859e52fe6933813afc9e7ee7
Dzmitry Fedarovich <dmitryf8@gmail.com>
2019-06-29 12:21:45 +0300

    Добавлена анимация совы

--- app/src/main/java/by/itacademy/pvt/homework/dz5/DZ5Activity.kt
+++ app/src/main/java/by/itacademy/pvt/homework/dz5/DZ5Activity.kt
@@ -1,7 +1,9 @@
 package by.itacademy.pvt.homework.dz5
 
 import android.app.Activity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import by.itacademy.pvt.homework.R
 import kotlinx.android.synthetic.main.activity_dz5.*
 
@@ -11,12 +13,21 @@ class DZ5Activity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5)
 
+        // заполняем список
         val diagramData = ArrayList<Item>()
         var item: Item
         for (i in 1..5) {
             item = Item(i, i.toString())
             diagramData.add(item)
         }
+
+        // передаем данные в диаграмму
         diagramDZ5.setList(diagramData)
+
+        // анимация
+        var animationImageView = findViewById<ImageView>(R.id.animationImageViewDZ5)
+        var animationDrawable: AnimationDrawable = animationImageView.background as AnimationDrawable
+
+        animationDrawable.start()
     }
 }
