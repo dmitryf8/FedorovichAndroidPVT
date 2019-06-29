38518c21f88c5b248f3d1b427a98b5323265ecd5
Dzmitry Fedarovich <dmitryf8@gmail.com>
2019-06-29 13:40:35 +0300

    Добавил DZ6Activity

---
+++ app/src/main/java/by/itacademy/pvt/homework/dz6/DZ6Activity.kt
@@ -0,0 +1,13 @@
+package by.itacademy.pvt.homework.dz6
+
+import android.app.Activity
+import android.os.Bundle
+import by.itacademy.pvt.homework.R
+
+class DZ6Activity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz6)
+    }
+}
