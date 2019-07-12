dbbdf92736ffbe48df055808323c2f5c6c83d75b
Dzmitry Fedarovich <dmitryf8@gmail.com>
2019-07-12 22:29:01 +0300

    DZ9 в процессе

---
+++ app/src/main/java/by/itacademy/pvt/homework/dz9/DZ9Activity.kt
@@ -0,0 +1,13 @@
+package by.itacademy.pvt.homework.dz9
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import by.itacademy.pvt.homework.R
+
+class DZ9Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz9)
+    }
+}
---
+++ app/src/main/java/by/itacademy/pvt/homework/dz9/entity/CoordinateParameters.kt
@@ -0,0 +1,6 @@
+package by.itacademy.pvt.myapplication.dz9.entity
+
+data class CoordinateParameters(
+    val coord1: Coordinate,
+    val coord2: Coordinate
+)
